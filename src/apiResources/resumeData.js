45f2b6fa20199cf3ae948c8e126951f6d177f3a4
Hossein <hossein.tb@gmail.com>
2020-11-23 03:49:29 -0500

    sset react environment variable

--- src/apiResources/resumeData.js
+++ src/apiResources/resumeData.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-
 export const getResumeData = async () => {
     try {
-        console.log("hossein", process.env.PUBLIC_URL);
-        const result = await axios.get(`${process.env.PUBLIC_URL}/api`);
+        console.log("hossein", endpoint);
+        const result = await axios.get(`${process.env.REACT_APP_API_URL}/api`);
         return result.data;
     } catch (err) {
         return Promise.reject(err);
