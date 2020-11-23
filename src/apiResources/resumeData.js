3f23ecf0c886178bc1e81c343630f52b7cf4736d
Hossein <hossein.tb@gmail.com>
2020-11-23 03:12:59 -0500

    added server proxy

--- src/apiResources/resumeData.js
+++ src/apiResources/resumeData.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import { PUBLIC_URL } from '../constants';
 
 export const getResumeData = async () => {
     try {
-        const result = await axios.get(`${PUBLIC_URL}/`);
+        const result = await axios.get(`/api`);
         return result.data;
     } catch (err) {
         return Promise.reject(err);
--- src/constants.js
+++
@@ -1,1 +0,0 @@
-export const PUBLIC_URL = 'http://www.hosseintorabi.com:50080';
